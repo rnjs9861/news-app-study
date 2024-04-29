acd85ed01091d5e4fba880376e672f84c8e099ec
rnjs9861 <rkwkvlrmakfh@gmail.com>
2024-04-29 10:53:28 +0900

    feat: 뉴스 외부 API 연동 실습 예제

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,23 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import React from "react";
 import { NavLink } from "react-router-dom";
+
+const CategoriesStyle = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  gap: 15px;
+  justify-content: center;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+    /* display: flex;
+    flex-wrap: wrap; */
+  }
+`;
 
 const categories = [
   { name: "all", text: "전체보기" },
@@ -11,18 +29,41 @@ const categories = [
   { name: "technology", text: "기술" },
 ];
 
+const NavLinkStyle = styled(NavLink)`
+  font-size: 1.125rem;
+  white-space: pre;
+  padding-bottom: 0.25rem;
+
+  &:hover {
+    color: #ababab;
+  }
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+  }
+
+  /* ${props =>
+    props.active &&
+    css`
+      font-weight: 600px;
+      border-bottom: 2px solid #22b8cf;
+      color: #22b8cf;
+    `} */
+`;
+
 const Categories = () => {
   return (
-    <div>
+    <CategoriesStyle>
       {categories.map(category => (
-        <NavLink
+        <NavLinkStyle
           key={category.name}
           to={category.name === "all" ? "/" : `/${category.name}`}
         >
           {category.text}
-        </NavLink>
+        </NavLinkStyle>
       ))}
-    </div>
+    </CategoriesStyle>
   );
 };
 
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -10,7 +10,7 @@ const NewsPage = () => {
 
   return (
     <div>
-      <h2>뉴스 목록 페이지입니다. </h2>
+      {/* <h2>뉴스 목록 페이지입니다. </h2> */}
       <Categories></Categories>
       <NewsList category={category}></NewsList>
     </div>
